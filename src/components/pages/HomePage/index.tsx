db0ed7459e0d331b11c173735348d5f90f63a9a0
meganpekarek <megan@livefront.com>
2023-04-10 19:49:22 -0500

    organize global styles and mixins

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -10,19 +10,19 @@ export default function Home() {
     <GridOverlay />
     <div className="row-container">
       <div className="row">
-        <div className="col-3-lg col-3-md col-6-sm exampleContent">Megan here!</div>
-        <div className="col-3-lg col-3-md col-6-sm exampleContent">Megan here!</div>
-        <div className="col-3-lg col-3-md col-6-sm exampleContent">Megan here!</div>
-        <div className="col-3-lg col-3-md col-6-sm exampleContent">Megan here!</div>
+        <div className="col-3-lg col-3-md col-2-sm exampleContent">Megan here!</div>
+        <div className="col-3-lg col-3-md col-2-sm exampleContent">Megan here!</div>
+        <div className="col-3-lg col-3-md col-2-sm exampleContent">Megan here!</div>
+        <div className="col-3-lg col-3-md col-2-sm exampleContent">Megan here!</div>
       </div>
       <div className="row">
         <div className="col-2-lg col-2-md col-2-sm exampleContent">Megan here!</div>
-        <div className="col-8-lg col-8-md col-8-sm exampleContent">Megan here!</div>
+        <div className="col-8-lg col-8-md col-4-sm exampleContent">Megan here!</div>
         <div className="col-2-lg col-2-md col-2-sm exampleContent">Megan here!</div>
       </div>
       <div className="row align-center">
-        <div className={`col-6-lg col-6-md col-12-sm ${styles.contentBlockLeft}`}>Block of image content</div>
-        <div className={`col-6-lg col-6-md col-12-sm ${styles.contentBlockRight}`}>Block of text content</div>
+        <div className={`col-6-lg col-6-md col-4-sm ${styles.contentBlockLeft}`}>Block of image content</div>
+        <div className={`col-6-lg col-6-md col-4-sm ${styles.contentBlockRight}`}>Block of text content</div>
       </div>
     </div>
     </>
