203cdc2af9c81d7fbf6dd67b7f6335bfa9e52e26
meganpekarek <megan@livefront.com>
2023-04-16 16:50:52 -0500

    reorganize hero section

--- src/components/pages/HomePage/HeroSection/index.tsx
+++ src/components/pages/HomePage/HeroSection/index.tsx
@@ -1,26 +1,15 @@
-import BlackoutText from "@/components/blocks/BlackoutText";
-import styles from "./index.module.scss";
+import ScrollingCategoriesBar from "@/components/blocks/ScrollingCategoriesBar";
+import SearchSection from "./SearchSection";
+import HeadingSection from "./HeadingSection";
 
 const HeroSection: React.FunctionComponent = () => {
   return (
-    <div className="row-container">
-      <div className={`row column-reverse-sm ${styles.content}`}>
-        <div className="col-4-lg col-4-md col-12-sm">
-          <img
-            className={styles.image}
-            src="/assets/photos/heroImage.png"
-            alt=""
-          />
-        </div>
-        <div
-          className={`col-8-lg col-8-md col-12-sm ${styles.textContent}`}
-        >
-          <h1>Unlocking the <BlackoutText text="possibly" /> true secrets of the <BlackoutText text="made up" /> universe:</h1>
-          <span className="subheading1">One <BlackoutText text="baseless" /> book at a time</span>
-        </div>
-      </div>
-    </div>
+    <>
+      <HeadingSection />
+      <SearchSection />
+      <ScrollingCategoriesBar />
+    </>
   );
-}
+};
 
-export default HeroSection
+export default HeroSection;
--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -2,7 +2,6 @@ import GridOverlay from "@/components/infrastructure/GridOverlay";
 import ScrollingCategoriesBar from "@/components/blocks/ScrollingCategoriesBar";
 import Header from "@/components/infrastructure/Header";
 import HeroSection from "./HeroSection";
-import SearchSection from "./SearchSection";
 import NewArrivalsSection from "./NewArrivalsSection";
 import DicksPicksSection from "./DicksPicksSection";
 import Footer from "@/components/infrastructure/Footer";
@@ -13,8 +12,6 @@ const HomePage: React.FunctionComponent = () => {
       <GridOverlay />
       <Header />
       <HeroSection />
-      <SearchSection />
-      <ScrollingCategoriesBar />
       <NewArrivalsSection />
       <DicksPicksSection />
       <Footer />
