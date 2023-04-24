0f5351dca056f57893712db26647392efd21f9e2
meganpekarek <megan@livefront.com>
2023-04-23 20:20:17 -0500

    cleanup

--- src/components/infrastructure/GridOverlay/index.tsx
+++ src/components/infrastructure/GridOverlay/index.tsx
@@ -1,14 +1,15 @@
 import styles from "./index.module.scss";
 
 const GridOverlay: React.FunctionComponent = () => {
-  const rows = [...new Array(12)];
-  const overlayRows = rows.map(() => (
-    <div className={`col-1 ${styles.overlayColumn}`}></div>
-  ));
+  const columns = [...new Array(12)];
 
   return (
     <div className={`${styles.container} row-container`}>
-      <div className={`row ${styles.overlayRow}`}>{overlayRows}</div>
+      <div className={`row ${styles.overlayRow}`}>
+        {columns.map(() => (
+          <div className={`col-1 ${styles.overlayColumn}`}></div>
+        ))}
+      </div>
     </div>
   );
 };
