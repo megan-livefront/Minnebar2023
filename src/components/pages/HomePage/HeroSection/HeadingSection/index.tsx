c58f3bc72cfe4a8fb38b87c3ddbe367d989d1821
meganpekarek <megan@livefront.com>
2025-01-09 18:44:33 -0600

    add fluid and spacing functions

--- src/components/pages/HomePage/HeroSection/HeadingSection/index.tsx
+++ src/components/pages/HomePage/HeroSection/HeadingSection/index.tsx
@@ -4,25 +4,25 @@ import styles from "./index.module.scss";
 const HeadingSection: React.FunctionComponent = () => {
   return (
     <div className="row-container">
-        <div className={`row column-reverse-sm ${styles.content}`}>
-          <div className={`${styles.imageWrapper} col-4-lg col-4-md col-12-sm`}>
-            <img
-              className={styles.image}
-              src="/assets/photos/heroImage.png"
-              alt=""
-            />
-          </div>
-          <div className={`col-7-lg col-7-md col-12-sm ${styles.textContent}`}>
-            <h1>
-              Unlocking the <BlackoutText text="possibly" /> true secrets of the{" "}
-              <BlackoutText text="made up" /> universe:
-            </h1>
-            <span className={`${styles.subheading} subheading1`}>
-              One <BlackoutText text="baseless" /> book at a time
-            </span>
-          </div>
+      <div className={`row column-reverse-sm ${styles.content}`}>
+        <div className={`${styles.imageWrapper} col-4-lg col-4-md col-2-sm`}>
+          <img
+            className={styles.image}
+            src="/assets/photos/heroImage.png"
+            alt=""
+          />
+        </div>
+        <div className={`col-7-lg col-7-md col-12-sm ${styles.textContent}`}>
+          <h1>
+            Unlocking the <BlackoutText text="possibly" /> true secrets of the{" "}
+            <BlackoutText text="made up" /> universe:
+          </h1>
+          <span className={`${styles.subheading} subheading1`}>
+            One <BlackoutText text="baseless" /> book at a time
+          </span>
         </div>
       </div>
+    </div>
   );
 };
 
