367a4372610b866532e60d967a2c9eccae4ea858
meganpekarek <megan@livefront.com>
2025-01-15 20:03:44 -0600

    cleanup

--- src/components/pages/HomePage/HeroSection/HeadingSection/index.tsx
+++ src/components/pages/HomePage/HeroSection/HeadingSection/index.tsx
@@ -15,7 +15,7 @@ const HeadingSection: React.FunctionComponent = () => {
         <div className={`col-7-lg col-7-md col-12-sm ${styles.textContent}`}>
           <h1>
             Unlocking the <BlackoutText text="possibly" /> true secrets of the{" "}
-            <BlackoutText text="made up" /> universe:
+            <BlackoutText text="made up" /> universe
           </h1>
           <span className={`${styles.subheading} subheading1`}>
             One <BlackoutText text="baseless" /> book at a time
